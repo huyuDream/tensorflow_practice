c928dc2f996e9d97af7174eea3b7b022fb330805
huyu <huyu@hungrystudio.com>
2025-09-10 20:06:32 +0800

    test

--- 2_first_model/first_model.py
+++ 2_first_model/first_model.py
@@ -1,43 +1,41 @@
-import tensorflow as tf # 导入TensorFlow
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 print("first model")
 
-# 加载mnist数据集
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# 生成线性数据
+np.random.seed(55) # 为了可重复性设置随机种子
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+x_data = np.linspace(-1, 1, 100)
+print(x_data)
+
+y_data = 2 * x_data + 1 + np.random.randn(100) * 0.33  # 生成 y 数据，y = 2x + 1，加上一些噪声
+print(y_data)
+
+# 一个输入层和一个输出层
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1, input_shape=(1,))
 ])
 
-print("predictions")
-predictions = model(x_train[:1]).numpy()
-predictions
+# 为优化器: 梯度下降（SGD）
+# 损失函数: 均方误差（MSE）
+model.compile(optimizer='sgd', loss='mse')
 
-print("softmax")
-tf.nn.softmax(predictions).numpy()
+# 训练模型
+model.fit(x_data, y_data, epochs=50)
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-loss_fn(y_train[:1], predictions).numpy()
-model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+# 评估模型
+loss = model.evaluate(x_data, y_data)
+print(f"Mean Squared Error: {loss}")
 
-model.fit(x_train, y_train, epochs=5)
+# 使用模型进行预测
+predictions = model.predict(x_data)
+print("Predictions:", predictions)
 
-model.evaluate(x_test,  y_test, verbose=2)
 
-
-probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
-
-probability_model(x_test[:5])
-
-# 保存模型
-#model.save("model.keras", save_format="tf") # keras 2.0
-model.export("./model_output/20250910/") # keras 3.0
+# plt.scatter(x_data, y_data, color='blue', label='Actual data')  # 绘制实际数据点
+# plt.plot(x_data, predictions, color='red', label='Fitted line')  # 绘制拟合线
+# plt.legend()
+# plt.show()
 
