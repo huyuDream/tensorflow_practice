221245a785768d047b901fd7b259ba1770f055eb
huyu <huyu@hungrystudio.com>
2025-09-10 11:05:05 +0800

    test

--- 2_first_model/first_model.py
+++ 2_first_model/first_model.py
@@ -20,3 +20,20 @@ predictions
 
 print("softmax")
 tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn(y_train[:1], predictions).numpy()
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
+
