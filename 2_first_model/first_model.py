1b16ec91d117b55252115d3cf9d855e77ad4a1a5
huyu <huyu@hungrystudio.com>
2025-09-10 13:59:39 +0800

    test

--- 2_first_model/first_model.py
+++ 2_first_model/first_model.py
@@ -38,6 +38,6 @@ probability_model = tf.keras.Sequential([
 probability_model(x_test[:5])
 
 # 保存模型
-model.save("model.keras")
+model.save("model.keras", save_format="tf")
 #model.export("./model_output/")
 
